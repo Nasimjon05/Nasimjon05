34401f502f2c2609f59ecc8934d12a4e74421d90
Nasimjon05 <155145157+Nasimjon05@users.noreply.github.com>
2023-12-29 13:47:51 +0500

    Update minigame.py
    This my first game

--- minigame.py
+++ minigame.py
@@ -1,1 +1,21 @@
-print('hello world')
+#mini_game
+import random
+a=random.randrange(0,10)
+b=int(input('Enter your number:'))
+
+l=[a,b]
+max_l=max(l)
+min_l=min(l)
+subtr=max_l-min_l
+
+if subtr==0:
+    print('Congratulations!!!You have won The game. It was '+str(a)+'.')
+elif subtr==1:
+    print('You are wrong with 1 number.You have one more opportunity')
+    another_chance=int(input('Enter your number:'))
+    if another_chance==a:
+        print('Congratulations!!!You have won The game. It was '+str(a)+'.')
+    else:
+        print('You could not find the number. It was '+str(a)+'.')
+else:
+    print('You could not find the number. It was ' + str(a) + '.')
